a190469e23092432341645e99064091ab70aab61
Spencer Williams <Spilliams@gmail.com>
2022-07-21 17:02:21 -0500

    Add 95% of the project because I forgot to commit features

--- script.js
+++ script.js
@@ -1,27 +1,107 @@
-
+let playerInput = "ROCK";
 
 
 function getComputerChoice() {
-    let random, choice;
+    let random;
 
     // generate a 1 - 3 choice of numbers
     random = Math.floor(Math.random() * 3) + 1;
 
     if (random == 1) {
-        console.log("Rock")
+        return("rock")
     }
     else if (random == 2) {
-        console.log("Paper")
+        return("paper")
     }
 
     else if (random == 3) {
-        console.log("Scissors")
+        return("scissors")
     }
 
     else {
-        console.log("Ain't got no gas in it!")
+        return("error")
     }
 
 }
 
-getComputerChoice();
+
+function rockPaperScissors(playerSelection, computerSelection) {
+
+    let outcome;
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        outcome = `You both chose ${playerSelection}, no winner!`;
+        return outcome;
+    }
+
+    // player winning logic
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        outcome = 'You win! Rock beats scissors.';
+        return outcome;
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        outcome = 'You win! Paper beats rock.';
+        return outcome;
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        outcome = 'You win! Scissors beats paper.';
+        return outcome;
+    }
+
+    // computer winning logic
+    else if (computerSelection == 'rock' && playerSelection == 'scissors') {
+        outcome = 'You lose! Rock beats scissors.';
+        return outcome;
+    }
+    else if (computerSelection == 'paper' && playerSelection == 'rock') {
+        outcome = 'You lose! Paper beats rock.';
+        return outcome;
+    }
+    else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+        outcome = 'You lose! Scissors beats paper.';
+        return outcome;
+    }
+
+    // error handling
+    else {
+        outcome = 'error!';
+        return outcome;
+    }
+}
+
+function game() {
+    let gameRound, winner;
+    let score = 0;
+    for (let i = 1; i <= 1; i++) {
+
+        let computerInput;
+        computerInput = getComputerChoice();
+
+        gameRound = rockPaperScissors(playerInput, computerInput);
+
+        if (gameRound.substring(0, 8) == 'You lose') {
+            score -= 1;
+        }
+        else {
+            score += 1;
+        }
+
+    }
+    
+    if (score == 0) {
+        winner = "You're both losers!";
+        console.log(winner);
+    }
+    else if (score > 0) {
+        winner = "You're a winner baby!";
+        console.log(winner);
+    }
+    if (score < 0) {
+        winner = "You lose chump!";
+        console.log(winner);
+    }
+
+}
+
+game()
