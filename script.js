10791de4632fc4cc446099f7fbcea6094b277d13
Spencer Williams <Spilliams@gmail.com>
2022-07-28 14:25:37 -0500

    Add game run to click action

--- script.js
+++ script.js
@@ -1,65 +1,31 @@
-let playerInput;
-let goodValues = ["rock", "paper", "scissors"];
-
+let score;
+// function for determining the computers choice in game
 function getComputerChoice() {
     let random;
-
     // generate a 1 - 3 choice of numbers
     random = Math.floor(Math.random() * 3) + 1;
-
+    // assign values to determine RPS choice
     if (random == 1) {
         return("rock")
     }
     else if (random == 2) {
         return("paper")
     }
-
     else if (random == 3) {
         return("scissors")
     }
-
     else {
         return("error")
     }
-
 }
-
-// use a click event to return the #id of the object
-function getID(click) {
-    let id;
-    click.stopPropagation();
-    id = this.id;
-    return id;
-}
-
-function playerChoice() {
-
-    let currentChoice;
-
-    // select all div elements
-    const divs = document.querySelectorAll('div');
-
-    // assign the players choice to the current clicked on div by using the
-    // getID function, setting capture to false so it doesn't select bubbled divs
-    currentChoice = divs.forEach(div => div.addEventListener('click', getID, {
-        capture: false
-    })); 
-    
-    return currentChoice;
-
-}
-
- 
+// function to determine who won the round
 function playRound(playerSelection, computerSelection) {
-
     let points;
-
     // if player and computer make same selection
     if (playerSelection === computerSelection) {
         points = 0;
         return points;
     }
-
     // player winning logic
     else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         points = 1;
@@ -73,7 +39,6 @@ function playRound(playerSelection, computerSelection) {
         points = 1;
         return points;
     }
-
     // computer winning logic
     else if (computerSelection == 'rock' && playerSelection == 'scissors') {
         points = -1;
@@ -87,11 +52,6 @@ function playRound(playerSelection, computerSelection) {
         points = -1;
         return points;
     }
-
-    else if (points = undefined) {
-        
-    }
-
     // error handling
     else {
         points = 'ERROR';
@@ -99,40 +59,19 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-
-    let winner;
-     // create variables for inputs
+// use a click event to return the #id of the object
+function game(e) {
     let computerInput, playerInput;
-    let score = 0;
-
-    // if score = 5, player wins
-    // if score = -5, computer wins
-    while (score < 5 || score > -5) {
-
-       
-        // get the computer and player inputs
-        computerInput = getComputerChoice();
-        playerInput = playerChoice();
-
-        console.log(computerInput);
-        console.log(playerInput);
-
-        score += playRound(playerInput, computerInput);
-
-        console.log(score);
-
-    }
-    
-    if (score == 5) {
-        winner = "You're a winner baby!";
-        console.log(winner);
-    }
-    else if (score == -5) {
-        winner = "You lose chump!";
-        console.log(winner);
-    }
-
+    e.stopPropagation();
+    playerInput = this.id;
+    computerInput = getComputerChoice();
+    console.log(playerInput);
+    console.log(computerInput);
 }
 
-game();
+// select all div elements
+const buttonType = document.querySelectorAll('button');
+
+const buttons = Array.from(buttonType);
+
+buttons.forEach(button => button.addEventListener('click', game, {capture: false}));
